f960730f73b4e5fe067d4af9ebad956a51e4eb27
greggythefly <gregoire@fournel.net>
2013-06-05 16:14:49 +0200

    preparing file organization

---
+++ php/init.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+function __autoload($class_name) {
+    if(file_exists('classes/'.$class_name . '.class.php')) {
+        require_once('classes/'.$class_name . '.class.php');
+    } else {
+        throw new Exception("Unable to load $class_name.");
+    }
+}
+
+/**
+ * Requirements
+ */
+require_once 'config/database.conf.php';
+require_once 'php/libs/smarty/Smarty.class.php';
+
+/**
+ * Library instanciation
+ */
+$pdo    = new PDO("mysql:host=" . DATABASE_HOST . ";dbname=" . DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD);
+$smarty = new Smarty;
